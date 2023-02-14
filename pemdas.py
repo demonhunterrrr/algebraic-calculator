167a9024c16a855e1cbe153686d8a558e5e12878
Max <max.steinberg.99@gmail.com>
2023-02-13 19:04:48 -0500

    me when the pemdas

--- pemdas.py
+++ pemdas.py
@@ -4,9 +4,10 @@ import re
 from numpy import prod
 
 REGEXS = {
-    "prod": r"-?\d+\.?\d?[*\/]+-?\d+\.?\d?",
+    "power": r"-?\d+\.?\d?\^-?\d+\.?\d?",
+    "prod": r"-?\d+\.?\d?[*\/]-?\d+\.?\d?",
     "sum": r"-?\d+\.?\d?[+-]+-?\d+\.?\d?",
-    "signs": r"(?<=\d)[+*\/-]"
+    "signs": r"(?<=\d)[+*\/^-]"
 }
 
 # removes unnecessary negative signs from a number
@@ -16,11 +17,12 @@ def check_for_double_negative(num):
         return num.replace("-","")
     return f'-{num.replace("-","")}'
 
-# converts to float and then sums or finds product
-def _repeated_operation(mode="sum", *args):
+# converts to float and then sums, finds product, or args[0] to the power of args[1]
+def _operation(mode="sum", *args):
     args = [float(i) for i in args]
     if mode == "sum": return sum(args)
-    return prod(args)
+    elif mode == "prod": return prod(args)
+    return args[0] ** args[1]
 
 # returns the main operation of an expression and its index if u want
 def get_symbol(expr: str, return_sign_index: bool):
@@ -37,7 +39,7 @@ def _basic_operations(expr: str, mode="prod"):
 
     # basically, don't delete this variable. I'm too stupid to explain what it is, but don't delete it.
     mode_change = 0
-    if mode == "prod": mode_change = 1
+    if mode != "sum": mode_change = 1
 
     # finding every operation in the entire expression
     operations = re.findall(REGEXS[mode], expr)
@@ -52,7 +54,7 @@ def _basic_operations(expr: str, mode="prod"):
         # this is just saying that if we're doing division, change the number on the left to its reciprocal.
         if symbol[0] == "/": number_on_left = 1 / float(number_on_left)
 
-        simplified_value = _repeated_operation(
+        simplified_value = _operation(
             mode,
             number_on_right,
             number_on_left)
@@ -60,7 +62,11 @@ def _basic_operations(expr: str, mode="prod"):
         operations = re.findall(REGEXS[mode], expr)
     return expr    
 
+def exponentiation(base, power):
+    return base ** power
+
 def calc(expr: str):
+    expr = _basic_operations(expr, "power")
     expr = _basic_operations(expr, "prod")
     expr = _basic_operations(expr, "sum")
     if re.search(r"[()]", expr): expr = calc(expr.replace("(","").replace(")",""))
